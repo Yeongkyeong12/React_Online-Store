e840924db783af5733d7e8891e5ee0758d02d104
Yeongkyeong12 <ohkyeong1995@gmail.com>
2021-12-06 10:53:23 +0900

    Deleted comments of products contents

--- src/App.js
+++ src/App.js
@@ -78,39 +78,6 @@ function App() {
           </Col>
         </Row>
       </Container>
-      {/* <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width="100%"
-              alt="product img"
-            />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            {" "}
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="100%"
-              alt="product img"
-            />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            {" "}
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="100%"
-              alt="product img"
-            />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
